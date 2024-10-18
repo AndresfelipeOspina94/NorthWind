50a1e8e80b9762c778740274493f59a7a3a7cc36
andres felipe ospina <afelipe.ospina@gmail.com>
2024-10-17 21:25:01 -0500

    Debug y Console Writers

--- NorthWind.ConsoleApp/Services/AppLogger.cs
+++ NorthWind.ConsoleApp/Services/AppLogger.cs
@@ -4,11 +4,12 @@ using NorthWind.Entities.ValueObjets;
 
 namespace NorthWind.ConsoleApp.Services;
 
-internal class AppLogger(IUserActionWriter writer)
+internal class AppLogger(IEnumerable<IUserActionWriter> writers)
 {
     public void WriteLog(string message)
     {
         UserAction Log = new UserAction("System",message);
-        writer.Write(Log);
+        foreach(var Writer in writers)
+        Writer.Write(Log);
     }
 }
